abf5da04ac9a9bef0fdd91b0ebc94046c0b526b5
Henricoramalho <henrico.ramalho46@gmail.com>
2025-09-29 08:35:27 -0300

    removido swagger

--- api/server.js
+++ api/server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../src/swagger.json');
 
 const routes = require('../src/routes');
 const port = process.env.PORT || 3000;
@@ -10,9 +8,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
   console.log('API executando em http://localhost:',port);
-  console.log(`Documentação em http://localhost:${port}/docs`);
 });
