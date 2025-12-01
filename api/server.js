e03a80a46e1d0f6e2483a242019f731609de8f9d
menegonlucas <lucasmenegon14@gmail.com>
2025-12-01 08:44:03 -0300

    Server e Prisma

--- api/server.js
+++ api/server.js
@@ -4,17 +4,10 @@ const routes = require('../src/routes');
 
 const port = process.env.PORT || 3001;
 const app = express();
-
-// Permitir requisições apenas do seu front-end
-app.use(cors({
-    origin: 'https://menegonlucas.github.io', // seu front-end
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-}));
-
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
-app.listen(port, () => {
-    console.log('API respondendo em http://localhost:' + port);
+app.listen(port, (req, res) => {
+    console.log('API respondendo em http://localhost:' + port)
 });
